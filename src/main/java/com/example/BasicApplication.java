259b9f04eb8449be4f5c2a6b2b8588ff24d87594
Isami <isamimitani@live.se>
2019-08-22 13:29:54 +0200

    first commit

---
+++ src/main/java/com/example/BasicApplication.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class BasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicApplication.class, args);
+                RestTemplate restTemplate = new RestTemplate();
+                 //omvandla från JSON:
+                //String res = restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random");
+                //System.out.println(res);
+	}
+}
